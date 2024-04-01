ff4e2a4e67695bd6690e78bb13b959b3a64635b3
rudetrue <austin.rude@truecos.com>
2024-04-01 10:59:39 -0600

    Show class labels on top of predicted bounding box

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -27,17 +27,18 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
         font = cv2.FONT_HERSHEY_SIMPLEX
 
         txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+        txt_height = cv2.getTextSize(text, font, 0.4, 1)[1]
         cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
 
         txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
         cv2.rectangle(
             img,
-            (x0, y0 + 1),
-            (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
+            (x0, y0),
+            (x0 + txt_size[0] + 1, y0 - int(1.5*txt_size[1])),
             txt_bk_color,
             -1
         )
-        cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+        cv2.putText(img, text, (x0, y0 - int((txt_size[1] * 0.5))), font, 0.4, txt_color, thickness=1)
 
     return img
 
